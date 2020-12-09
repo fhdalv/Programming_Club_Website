70eb5d160fc58fa9e4d8c9e163df416a24647656
fhdalv <fahad994@gmail.com>
2020-12-09 03:59:31 -0500

    fixed the bug on email validation.

--- contact.js
+++ contact.js
@@ -49,5 +49,5 @@ function setSuccessFor(input) {
 }
 
 function isEmail(email) {
-	return (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(con_email));
+	return (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email));
 }
